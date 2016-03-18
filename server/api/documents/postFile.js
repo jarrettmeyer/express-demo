f225d5bac6d9e00b56624d03a0b8822c0bcc11cb
Jarrett Meyer <jarrettmeyer@gmail.com>
2016-03-18 10:00:39 -0400

    Code cleanup for POST /api/documents

--- server/api/documents/postFile.js
+++ server/api/documents/postFile.js
@@ -3,23 +3,25 @@
 const debug = require('debug')('server');
 const documents = require('../../services/documents');
 const saveFile = require('../../services/files/saveFile');
+const _ = require('lodash');
 
 module.exports = (request, response) => {
-  var fileData = request.file;
-  return saveFile(fileData.buffer.data, request.user)
+  let responseDocument = null;
+  return saveFile(request.file.buffer.data, request.user)
     .then(saveFileResult => {
-      let documentData = {
-        id: request.document.id,
-        title: request.document.title,
-        abstract: request.document.abstract,
-        originalFilename: fileData.originalname,
-        path: saveFileResult.path,
-        type: fileData.mimetype,
-        published: request.document.published
-      };
+      let documentData = getDocumentData(request.document, request.file, saveFileResult);
       return documents.update(documentData);
     })
     .then(updateDocumentResult => {
-      return response.status(200).json({ document: updateDocumentResult.toJSON() });
+      responseDocument = updateDocumentResult;
+      return response.status(200).json({ document: responseDocument.toJSON() });
     });
+}
+
+function getDocumentData(document, fileData, saveFileResult) {
+  return _.assign(document, {
+    originalFilename: fileData.originalname,
+    type: fileData.mimetype,
+    path: saveFileResult.path
+  });
 }
