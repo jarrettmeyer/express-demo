af16be6f22c89900e058358194b43d490e2b28aa
Jarrett Meyer <jarrettmeyer@gmail.com>
2016-03-23 10:18:58 -0400

    Moves request body blacklist to a config.json file.

--- server/utils/logRequests.js
+++ server/utils/logRequests.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const assert = require('assert');
+const config = require('../../config.json');
 const debug = require('debug')('api');
 const removeProperty = require('./removeProperty');
 const _ = require('lodash');
 
-const blacklist = ['password', 'passwordConfirmation'];
+const blacklist = config.blacklist;
 
 module.exports = () => {
   return function (request, response, next) {
@@ -18,6 +20,7 @@ module.exports = () => {
 // Log the request body. Do not log any body properties that appear in the blacklist.
 function logRequestBody(request) {
   let body = _.cloneDeep(request.body);
+  assert.ok(blacklist && blacklist.length > 0, 'Expected to have a request body blacklist, but none was found.');
   removeProperty(body, blacklist);
   if (body && Object.keys(body).length) {
     debug(`  >> Request Body: ${JSON.stringify(body)}`);
